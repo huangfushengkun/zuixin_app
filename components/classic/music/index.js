857d9788c7bdd710f8f7f87e140d2f2dbd488391
xzh.hf <xzh.hf@hangshutech.com>
2019-06-27 16:56:04 +0800

    V1.0.0添加音乐控件

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,6 +1,8 @@
 // components/classic/music/index.js
 import {classicBeh} from '../classic-beh.js'
 
+const mMgr = wx.getBackgroundAudioManager()
+
 Component({
   /**
    * 组件的属性列表
@@ -8,13 +10,15 @@ Component({
   behaviors: [classicBeh],  //可以继承多个
 
   properties: {
-    src:String
+    src:String,
+    title:String
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    playing:false,
     pauseSrc: 'images/player@waitting.png',
     playSrc: 'images/player@playing.png'
   },
@@ -23,6 +27,13 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onPlay:function (event) {
+      this.setData({
+        playing:!this.data.playing
+      })
+      console.log(this.properties.src)
+      mMgr.src = this.properties.src
+      mMgr.title = this.properties.title
+    }
   }
 })
