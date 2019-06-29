94a92a3d5e9e6df3df6d5cb44f60f2464639bc02
xzh.hf <xzh.hf@hangshutech.com>
2019-06-29 11:24:34 +0800

    添加书籍详情页

--- models/book.js
+++ models/book.js
@@ -8,6 +8,32 @@ class BookModel extends HTTP {
             url:'book/hot_list'
         })
     }
+    // 获取点赞状态
+    getMyBookCount() {
+      return this.request({
+        url:'/book/favor/count'
+      })
+    }
+    //获取书籍详细信息
+    getDetail(bid) {
+      return this.request({
+        url:`book/${bid}/detail`
+      })
+    }
+      // 获取当前书籍的点赞状态
+    getLikeStatus (bid) {
+      return this.request({
+        url:`book/${bid}/favor`
+      })
+    }
+
+    // 书籍的短评信息
+    getComments(bid) {
+      return this.request({
+        url:`book/${bid}/short_comment`
+      })
+    }
+
 }
 
 export {BookModel}
---
+++ pages/book-detail/book-detail.js
@@ -0,0 +1,92 @@
+// pages/book-detail/book-detail.js
+import {
+  BookModel
+} from '../../models/book.js'
+const bookModel = new BookModel()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    book:{}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const bid = options.bid
+    const detail = bookModel.getDetail(bid)
+    const comments = bookModel.getComments(bid)
+    const likeStatus = bookModel.getLikeStatus(bid)
+
+    detail.then(res => {
+      this.setData({
+        book:res
+      })
+    })
+
+    comments.then(res => {
+
+    })
+
+    likeStatus.then( res => {
+
+    })
+    
+    console.log(detail)
+    console.log(comments)
+    console.log(likeStatus)
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
